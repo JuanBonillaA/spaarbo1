a205f003ce83c059908bf13ceb8bd195714f6298
JuanBonillaA <juan.bonilla.mt@usa.edu.co>
2022-12-20 14:45:51 -0500

    proyecto terminado

---
+++ src/connetions/endpoints.js
@@ -0,0 +1,4 @@
+const API= "https://fdd0303c-c00d-49bb-b976-624c50615012.mock.pstmn.io"
+
+export const PRODUCTOSCREADOS_GET_ENDPOINT= API + "/producto"
+export const CREARPRODUCTO_POST_ENDPOINT= API + "/producto"
